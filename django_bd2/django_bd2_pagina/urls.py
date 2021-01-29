f8f2d97dca399be09cd34b17af17a7fad6abe47b
Gonçalo <goncalo.costa.22000@gmail.com>
2021-01-29 16:17:36 +0000

    erro

--- django_bd2/django_bd2_pagina/urls.py
+++ django_bd2/django_bd2_pagina/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path
+from django.urls import path,include
 from . import views
 urlpatterns = [
     path('', views.index, name='index'),
-    path('create', views.create),
+    path('create', views.create, name='create'),
     path('list', views.list, name='index'),
     path('update',views.update ,name='index')
 ]
--- django_bd2/django_bd2_pagina/views.py
+++ django_bd2/django_bd2_pagina/views.py
@@ -1,16 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
+from django.views.decorators.csrf import csrf_exempt #ignorar os tokens, etc
+from django.template import loader
+
+#json encoder
+from django.core.serializers.json import DjangoJSONEncoder
+from json import dumps as json_dumps
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render({},request))
 
 def create(request):
-    return HttpResponse("Página de create")
+    template = loader.get_template('create.html')
+  
+    return render(request,'create.html',{})
 
 def list(request):
-    return HttpResponse("Página de list")
+    template = loader.get_template('update.html')
+  
+    return HttpResponse(template.render({},request))
 
 def update(request):
-    return HttpResponse("Página de update")
+    template = loader.get_template('list.html')
+  
+    return HttpResponse(template.render({},request))
 
 
